db15dc0a7b0f820a5b818116e5c1a8fae8d0d412
milochen0418 <milo_chen@mail2000.com.tw>
2023-09-20 21:46:51 +0800

    fix: fix bug to run well for python-cli

--- python-cli/README.md
+++ python-cli/README.md
@@ -6,4 +6,5 @@ pip install -r requirements.txt
 Don't need to compile the python's code. 
 
 ## Execute
-python wol.py -m XX:XX:XX:XX:XX:XX -b 192.168.1.255
+python wol.py -m "aa:bb:aa:bb:aa:bb" 
+python wol.py -m "aa:bb:aa:bb:aa:bb" -b "192.168.1.255"
--- python-cli/wol.py
+++ python-cli/wol.py
@@ -4,11 +4,6 @@ import argparse
 def send_magic_packet(mac_address, broadcast_address='255.255.255.255', port=9):
     """
     Send a Wake-On-LAN magic packet to the specified MAC address.
-    
-    Parameters:
-    - mac_address (str): The MAC address to send the magic packet to.
-    - broadcast_address (str): The broadcast address to send the packet to. Defaults to '255.255.255.255'.
-    - port (int): The port to send the packet to. Defaults to 9.
     """
     # Construct the magic packet
     mac_bytes = [int(byte, 16) for byte in mac_address.split(':')]
@@ -20,16 +15,11 @@ def send_magic_packet(mac_address, broadcast_address='255.255.255.255', port=9):
         sock.sendto(data, (broadcast_address, port))
 
 if __name__ == "__main__":
-    # Parse the arguments.     
-    parser.add_argument('-b', '--broadcastaddress', required=True, help='Broadcast IP Address')
-    parser.add_argument('-m', '--macaddress', required=True, help='MAC Address')
-    args = parser.parse_args()
-
     # Parsing command line arguments
     parser = argparse.ArgumentParser(description='Send a Wake-On-LAN magic packet.')
-    parser.add_argument('-m' '--macaddress', help='Target MAC address to send the magic packet to (format: XX:XX:XX:XX:XX:XX).')
+    parser.add_argument('-m', '--macaddress', required=True, help='Target MAC address to send the magic packet to (format: XX:XX:XX:XX:XX:XX).')
     parser.add_argument('-b', '--broadcastaddress', default='255.255.255.255', help='Broadcast address to send the packet to. Defaults to 255.255.255.255.')
     args = parser.parse_args()
 
-    send_magic_packet(args.mac_address, args.broadcast)
-    print(f"Sent WOL packet to {args.mac_address} via {args.broadcast}")
+    send_magic_packet(args.macaddress, args.broadcastaddress)
+    print(f"Sent WOL packet to {args.macaddress} via {args.broadcastaddress}")
